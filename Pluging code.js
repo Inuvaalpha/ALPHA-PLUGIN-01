c3cbbf72de9bd1fca6dad5d9a38b77c2413b45a0
Inuvaalpha <100457894+Inuvaalpha@users.noreply.github.com>
2022-02-28 17:02:13 +0530

    Update Pluging code.js

--- Pluging code.js
+++ Pluging code.js
@@ -9,9 +9,9 @@ if (Config.WORKTYPE == 'public') {
 
     Alpha.addrex({pattern: 'cmd', fromMe: true,  deleteCommand: false }, (async (message, match) => {
 
-            var image = await axios.get ('https://telegra.ph/file/cd6032c65c27e0510ddad.jpg', {responseType: 'arraybuffer'})//ඔය ලින්ක් එකට  ඕන ෆොටෝ එකේ ලින්ක් එක දන්න. 💫
+            var image = await axios.get ('https://i.ibb.co/hsz5q3S/Main-LOgo.jpg', {responseType: 'arraybuffer'})//ඔය ලින්ක් එකට  ඕන ෆොටෝ එකේ ලින්ක් එක දන්න. 💫
 
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: ` Any Massage Tyep`,quoted: message.data})//ඔතන  ඕන මැස්වෙජ් එක දාන්න.💫
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: ` Inuka Tech`,quoted: message.data})//ඔතන  ඕන මැස්වෙජ් එක දාන්න.💫
 
     }));
     
