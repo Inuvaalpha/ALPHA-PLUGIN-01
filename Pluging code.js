1270b30cc929acfc5c14a84e70d9267c10f3ba61
Dark-Maxv1 <98449027+Dark-Maxv1@users.noreply.github.com>
2022-02-28 13:32:38 +0530

    Create Pluging code.js

---
+++ Pluging code.js
@@ -0,0 +1,7 @@
+/* # Exclusively from Neotro Project 
+# Do not use this fore any commercial thing
+# Do not edit (Respect to the Devaoloper) 
+# All rights reserved ®Teenuh-X @Teenuh Wa.me/+94766598862
+# Credit : usufusta-whatsappAsena
+*/
+
